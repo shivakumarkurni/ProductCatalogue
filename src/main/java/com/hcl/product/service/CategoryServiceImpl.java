d0b6011a7848f6e5e11d7ed2be1c57b80e6cbde4
manisha <manisha9233@gmail.com>
2019-08-27 13:07:31 +0530

    Categories API Commited by Lakshmi

---
+++ src/main/java/com/hcl/product/service/CategoryServiceImpl.java
@@ -0,0 +1,38 @@
+package com.hcl.product.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hcl.product.dto.CategoryDetailsDto;
+import com.hcl.product.entity.Category;
+import com.hcl.product.repository.CategoryRepository;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CategoryServiceImpl.class);
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+
+	public List<CategoryDetailsDto> categories() {
+		LOGGER.debug("CategoryServiceImpl categories()");
+		List<CategoryDetailsDto> categoryDetailsDtos = new ArrayList<CategoryDetailsDto>();
+		List<Category> categories = categoryRepository.findAll();
+
+		for (Category category : categories) {
+			CategoryDetailsDto categoryDetailsDto = new CategoryDetailsDto();
+			BeanUtils.copyProperties(category, categoryDetailsDto);
+			categoryDetailsDtos.add(categoryDetailsDto);
+		}
+
+		return categoryDetailsDtos;
+	}
+
+}
